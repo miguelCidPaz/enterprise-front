7bf3c55eaa9ac7c539f44cc7b7b4aead0660ec14
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-02-14 17:53:23 +0100

    mejoradas animaciones header

--- src/components/Header/internal-components/MenuLinks.jsx
+++ src/components/Header/internal-components/MenuLinks.jsx
@@ -3,9 +3,9 @@ const MenuLinks = () => {
     return (
         <nav className="bodyheader--menu-main">
             <div className="bodyheader--menu-container">
-                <p className="bodyheader--menu-link">Registro</p>
-                <p className="bodyheader--menu-link">Ranking</p>
-                <p className="bodyheader--menu-link">Placeholder</p>
+                <p className="bodyheader--menu-link link-1">Registro</p>
+                <p className="bodyheader--menu-link link-2">Ranking</p>
+                <p className="bodyheader--menu-link link-3">Placeholder</p>
             </div>
             <div className='bodyheader--frame-menu-mobile'>
                 <div className='bodyheader--menu-mobile'></div>
