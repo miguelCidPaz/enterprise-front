c24d46ca16542eebcda8e5e1b7f58ba2d5cce5d3
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-02-15 21:34:13 +0100

    punto de control

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,15 +1,42 @@
 import './styles.scss';
 
-const Filter = () => {
+const Filter = (props) => {
 
     return (
         <section className={`filter--main`}>
+
             <button className={`filter--button-show`}>
                 <p className={`filter--button-show-label`}>Filter</p>
             </button>
-            <form action="">
 
-            </form>
+            <div className={`filter--container`}>
+
+                <div className={`filter--container-button`}>
+                    <button className={`filter--button-deactivate ${props.theme}`}>on</button>
+                    <button>Asc</button>
+                    <p>Por valor</p>
+                </div>
+
+                <div className={`filter--container-button`}>
+                    <button className={`filter--button-deactivate ${props.theme}`}>on</button>
+                    <button>Asc</button>
+                    <p>Numero de empleados</p>
+                </div>
+
+                <div className={`filter--container-button`}>
+                    <button className={`filter--button-deactivate ${props.theme}`}>on</button>
+                    <button>Asc</button>
+                    <p>Eventos mas proximos</p>
+                </div>
+
+                <div className={`filter--container-button`}>
+                    <button className={`filter--button-deactivate ${props.theme}`}>on</button>
+                    <button>Asc</button>
+                    <p>Antiguedad en la web</p>
+                </div>
+
+            </div>
+
         </section>
     )
 }
