29f07d435d86a3283f8b55a3270131d4843fb6ef
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-02-16 19:06:41 +0100

    terminado visual y seleccion de opcion

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,39 +1,51 @@
+import { useState, useEffect } from 'react';
 import './styles.scss';
 
 const Filter = (props) => {
+    const [filter, setFilter] = useState({
+        values: false, employeds: true, events: false, times: false
+    });
+
+    const [show, setShow] = useState(false);
+
+    const changeFilter = (value) => {
+        const copy = { values: false, employeds: false, events: false, times: false };
+        if (value !== undefined) {
+            copy[value] = true;
+        }
+        setFilter(copy);
+    }
+
+    useEffect(() => {
+    }, [filter]);
+
+
+    const options = [
+        { option: 'Por valor de empresa', value: 'values' },
+        { option: 'Numero de empleados', value: 'employeds' },
+        { option: 'Eventos mas proximos', value: 'events' },
+        { option: 'Antiguedad en la web', value: 'times' },
+    ]
 
     return (
-        <section className={`filter--main`}>
+        <section className={`filter--main ${props.theme}`} className={show ? `filter--main-show ${props.theme}` : `filter--main ${props.theme}`}>
 
-            <button className={`filter--button-show`}>
+            <button onClick={e => setShow(!show)} className={`filter--button-show`}>
                 <p className={`filter--button-show-label`}>Filter</p>
             </button>
 
             <div className={`filter--container`}>
 
-                <div className={`filter--container-button`}>
-                    <button className={`filter--button-deactivate ${props.theme}`}>on</button>
-                    <button>Asc</button>
-                    <p>Por valor</p>
-                </div>
-
-                <div className={`filter--container-button`}>
-                    <button className={`filter--button-deactivate ${props.theme}`}>on</button>
-                    <button>Asc</button>
-                    <p>Numero de empleados</p>
-                </div>
-
-                <div className={`filter--container-button`}>
-                    <button className={`filter--button-deactivate ${props.theme}`}>on</button>
-                    <button>Asc</button>
-                    <p>Eventos mas proximos</p>
-                </div>
-
-                <div className={`filter--container-button`}>
-                    <button className={`filter--button-deactivate ${props.theme}`}>on</button>
-                    <button>Asc</button>
-                    <p>Antiguedad en la web</p>
-                </div>
+                {options.map(element => {
+                    return (
+                        <div className={`filter--container-button`}>
+                            <button onClick={e => changeFilter(element.value)} className={`${filter[element.value] ? 'filter--enable' : 'filter--disable'} filter--button ${props.theme}`}>
+                                <div className={`${filter[element.value] ? 'filter--pilot-enable' : 'filter--pilot-disable'}`}></div>
+                            </button>
+                            <p className={`filter--label-button ${props.theme}`}>{element.option}</p>
+                        </div>
+                    )
+                })}
 
             </div>
 
