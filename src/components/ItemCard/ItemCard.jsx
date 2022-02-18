0bf27a74e1dcbe6cb41c057e12e39cfb33707001
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-02-18 20:51:43 +0100

    save para ir al back

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,12 +1,21 @@
-const ItemCard = () => {
+import './styles.scss';
+
+const ItemCard = (props) => {
 
     return (
-        <div className="itemcard--main">
-            <div className="itemcard--body-left">
-
+        <div className={`itemcard--main ${props.theme}`}>
+            <div className={`itemcard--body-container ${props.theme}`}>
+                <div className={`itemcard--body-left ${props.theme}`}>
+                    <p className={`itemcard--item-name ${props.theme}`}>Soy un nombre medio largo</p>
+                    <p className={`itemcard--item-site ${props.theme}`}>https://www.ascodevida-enterprise.com/</p>
+                    <p className={`itemcard--item-employeds ${props.theme}`}>259</p>
+                </div>
+                <div className={`itemcard--body-right ${props.theme}`}>
+                    <img src="https://www.raccoongames.es/med/img/productos/2017/08/15/tp%20POP_17.jpg" alt="Brand" />
+                </div>
             </div>
-            <div className="itemcard--body-right">
-
+            <div className={`itemcard--body-button ${props.theme}`}>
+                <button className={`itemcard--button ${props.theme}`}>Details</button>
             </div>
         </div>
     )
--- src/components/TableItems/TableItems.jsx
+++ src/components/TableItems/TableItems.jsx
@@ -1,18 +1,25 @@
+import ItemCard from '../ItemCard/ItemCard';
 import './styles.scss';
 
-const TableItems = () => {
+const TableItems = (props) => {
 
     const numbers = [1, 2, 3, 4, 5, 6]
     return (
         <section className="tableitems--main">
             <div className="tableitems--interior-body">
-
+                {numbers.map(e => {
+                    return (
+                        <ItemCard theme={props.theme} />
+                    )
+                })}
             </div>
             <div className="tableitems--pagination">
                 {numbers.map(e => {
-                    <div className="tableitems--pagination-container">
-                        <p>{e}</p>
-                    </div>
+                    return (
+                        <div className="tableitems--pagination-container">
+                            <p>{e}</p>
+                        </div>
+                    )
                 })}
             </div>
         </section>
