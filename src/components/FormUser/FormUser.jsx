27f03a506176598f7259ed744de1b4bba5b653b2
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-02-18 15:55:41 +0100

    progress user

--- src/components/FormUser/FormUser.jsx
+++ src/components/FormUser/FormUser.jsx
@@ -1,7 +1,7 @@
 
-import React , { useState , useEffect, useContext } from 'react';
-import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import React from 'react';
+//import { useForm } from 'react-hook-form';
+//import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
@@ -10,19 +10,21 @@ import './styles.scss';
  * @params theme
  * @returns component react
  */
- export default function FormUser(props) {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+export default function FormUser(props) {
+    //const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
-    console.log(errors);
+    //console.log(errors);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Login" {...register("Login", {required: true, maxLength: 80})} />
-            <input type="password" placeholder="Password" {...register("Password", {required: true, maxLength: 100})} />
-            <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-
-            <input type="submit" />
-        </form>
+        <div className='form--register'>
+            <form onSubmit={onSubmit}>
+                <label>name</label>
+                <input type="text" />
+                {/* <input type="text" placeholder="Login" {...register("Login", {required: true, maxLength: 80})} />
+                <input type="password" placeholder="Password" {...register("Password", {required: true, maxLength: 100})} />
+                <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />*/}
+                <input type="submit" />
+            </form>
+        </div>
     );
-
- }
+}
