0bc9f0b0a821673b844c0d331e19c756821ecc94
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-02-19 12:13:26 +0100

    progress password

--- src/components/FormUser/FormUser.jsx
+++ src/components/FormUser/FormUser.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import md5 from 'md5';
 
@@ -11,16 +11,50 @@ import './styles.scss';
  * @returns component react
  */
 export default function FormUser(props) {
-    const { register, handleSubmit, formState: { errors } } = useForm(); //
-    const onSubmit = (data) => {console.log(data); alert(JSON.stringify(data));}; //preventDefault(); 
+    //const validationOpt = { resolver: yupResolver(formSchema) }
+    const { register, handleSubmit, watch, formState: { errors } } = useForm(); 
+    /* const password = useRef({});
+    console.log(md5(password)); */
+    let pwd = watch("password");
+    console.log(pwd);
+    const onSubmit = async (data) => {console.log(JSON.stringify(data)); alert(JSON.stringify(data));}; 
     console.log(errors);
 
-    return (
+    return (<>
         <form className='form--register' onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder="Login" {...register("Login", {required: true, maxLength: 80})} />
             <input type="password" placeholder="Password" {...register("Password", {required: true, maxLength: 100})} />
             <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
             <input type="submit" />
         </form>
+        {/* <form onSubmit={e => e.preventDefault()}>
+            <label>Password</label>
+            <input
+                name="password"
+                type="password"
+                ref={register({
+                    required: "You must specify a password",
+                    minLength: {
+                    value: 8,
+                    message: "Password must have at least 8 characters"
+                    }
+                })}
+                />
+            {errors.password && <p>{errors.password.message}</p>}
+    
+            <label>Repeat password</label>
+            <input
+                name="password_repeat"
+                type="password"
+                ref={register({
+                    validate: value =>
+                    value === password.current || "The passwords do not match"
+                })}
+                />
+            {errors.password_repeat && <p>{errors.password_repeat.message}</p>}
+    
+            <input type="submit" onClick={handleSubmit(onSubmit)} />
+        </form> */}
+        </>
     );
 }
