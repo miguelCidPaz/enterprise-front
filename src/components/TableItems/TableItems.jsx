9ca8c6a771f66a9359efc615645cfdb249275af3
Miguel Cid <miguel.cid.paz@gmail.com>
2022-02-24 16:54:21 +0100

    solucionado bug filter y animado menu paginacion

--- src/components/TableItems/TableItems.jsx
+++ src/components/TableItems/TableItems.jsx
@@ -11,7 +11,7 @@ const TableItems = (props) => {
 
     const numbers = [1, 2, 3, 4, 5, 6]
     const halfNumbers = Math.round(numbers.length / 2);
-    console.log(halfNumbers)
+
     return (
         <section className="tableitems--main">
             <div className="tableitems--interior-body">
