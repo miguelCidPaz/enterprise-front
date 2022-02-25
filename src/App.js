f5064dd1ef416f3f4f9c5770915521da0cf28eb6
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-02-25 16:12:58 +0100

    form enterprise done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import BodyHeader from "./components/Header/BodyHeader";
 import LandPage from "./components/LandPage/LandPage";
 import Ranking from './components/Ranking/Ranking';
 import Login from './components/Login/Login';
+import FormEnterprise from './components/FormEnterprise/FormEnterprise';
 import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
@@ -27,6 +28,7 @@ const App = () => {
         <Route path='/' element={<LandPage theme={theme} />} />
         <Route path='/ranking' element={<Ranking theme={theme} />} />
         <Route path='/login' element={<Login theme={theme} />} />
+        <Route path='/FormEnterprise' element={<FormEnterprise theme={theme} />} />
       </Routes>
     </div>
 
