8003cf4f9e90a1acfe7a7a1acb3f785d0841030f
Miguel Cid <miguel.cid.paz@gmail.com>
2022-03-01 16:44:07 +0100

    Perfil para acceder al crud de empresa

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -3,7 +3,25 @@ import './styles.scss';
 const Perfil = () => {
 
     return (
-        <div>Perfil</div>
+        <section className='perfil--main'>
+            <div className='perfil--main-container'>
+                <div className='perfil--slot'>
+                    <p className='perfil--label'>Your Name: </p>
+                    <p className='perfil--text'>MyName</p>
+                </div>
+                <div className='perfil--slot'>
+                    <p className='perfil--label'>Your Email: </p>
+                    <p className='perfil--text'>MyEmail</p>
+                </div>
+
+
+                <p className='perfil--text perfil--title'>Acciones</p>
+
+                <button className='perfil--button'>Crear una Empresa</button>
+                <button className='perfil--button-left'>Ver tus Empresa</button>
+                <button className='perfil--button'>Borrar Empresa</button>
+            </div>
+        </section>
     )
 }
 
