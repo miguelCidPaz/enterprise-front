3219c807217e75d71c30026eb4664352aa54d026
Miguel Cid <miguel.cid.paz@gmail.com>
2022-03-03 17:21:38 +0100

    añadido view de empresas desde profile y fixeados algunos estilos problematicos en itemcard y profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+import ItemCard from '../ItemCard/ItemCard';
 import './styles.scss';
 
 const Profile = () => {
+    const [viewItems, setViewItems] = useState(false);
 
+    const arr = [1, 2, 3, 4, 5]
     return (
         <section className='profile--main'>
-            <div className='profile--main-container'>
+            {viewItems ? <div className='profile--main-enterprises'>
+                <div className="profile--main-center">
+                    {arr.map((e, i) => {
+                        return <ItemCard key={i} index={i} />
+                    })}
+                    <button className='profile--button-left profile--button-center' onClick={e => setViewItems(!viewItems)}>Volver a perfil</button>
+                </div>
+            </div> : <div className={viewItems ? 'profile--main-container profile--main-container-toright' : 'profile--main-container'}>
                 <div className='profile--slot'>
                     <p className='profile--label'>Your Name: </p>
                     <p className='profile--text'>MyName</p>
@@ -17,9 +28,10 @@ const Profile = () => {
 
                 <p className='profile--text profile--title'>Acciones</p>
 
-                <button className='profile--button-left'>Ver tus empresas</button>
+                <button className='profile--button-left' onClick={e => setViewItems(!viewItems)}>Ver tus empresas</button>
                 <button className='profile--button'>Log Out</button>
-            </div>
+            </div>}
+
         </section>
     )
 }
