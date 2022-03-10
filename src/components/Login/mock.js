a118c23469f83a1f76a8ee601e7f7d5e624b920e
Miguel Cid <miguel.cid.paz@gmail.com>
2022-03-10 14:01:06 +0100

    retocado provider para recoger el id

--- src/components/Login/mock.js
+++ src/components/Login/mock.js
@@ -1,5 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export const users = {
     1: {
+        id: uuidv4(),
         userName: "admin",
         userMail: "admin@outlook.toprop",
         userPass: "123"
