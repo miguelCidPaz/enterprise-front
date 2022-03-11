08cf3071904daeebb55d96bc73f40007a05302cc
Miguel Cid <53853619+miguelCidPaz@users.noreply.github.com>
2022-03-11 11:28:52 +0100

    Todos los classnames

--- src/components/FormEnterprise/FormEnterprise.jsx
+++ src/components/FormEnterprise/FormEnterprise.jsx
@@ -22,43 +22,43 @@ export default function FormEnterprise(props) {
         <div className='form--main'>
             <form className='form--login' onSubmit={handleSubmit(onSubmit)} >
                 {/* Nombre */}
-                <input type="text" placeholder="Nombre" {
+                <input className='form--input' type="text" placeholder="Nombre" {
                     ...register("Nombre", 
                     {required: {value: true, message:'Campo requerido'},
                     maxLength: {value: 80, message:'Tamaño maximo 80'}})} />
                 {errors.Nombre && <p className='message--errors'>{errors.Nombre.message}</p>}
                 {/* Sector */}
-                <input type="text" placeholder="Sector" {
+                <input className='form--input' type="text" placeholder="Sector" {
                     ...register("Sector", {required: false,
                     maxLength: {value: 100, message:'Tamaño maximo 100'}})} />
                 {errors.Nombre && <p className='message--errors'>{errors.Nombre.message}</p>}
                 {/* Fecha creacion */}
                 {/* Es posible q la fecha solo incluya mes/año o solo año o q me toquen las pelotas con otros formatos? */}
-                <input type="datetime" placeholder="Fecha Creacion" {
+                <input className='form--input' type="datetime" placeholder="Fecha Creacion" {
                     ...register("Fecha Creacion", {required: false})} /> 
                 {/* Logotipo */}
-                <input type="url" placeholder="Logotipo" {
+                <input className='form--input' type="url" placeholder="Logotipo" {
                     ...register("Logotipo", {required: false})} />
                 {/* Pagina Web */}
-                <input type="url" placeholder="Pagina Web" {
+                <input className='form--input' type="url" placeholder="Pagina Web" {
                     ...register("Pagina Web", {required: false})} />
                 {/* Numero de Telefono */}
-                <input type="number" placeholder="Numero de Telefono" {
+                <input className='form--input' type="number" placeholder="Numero de Telefono" {
                     ...register("Numero de Telefono", {required: false})} />
                 {/* Social Media */}
-                <input type="text" placeholder="Social Media" {
+                <input className='form--input' type="text" placeholder="Social Media" {
                     ...register("Social Media", {required: false})} />
                 {/* Valoracion */}
-                <input type="number" placeholder="Valoracion" {
+                <input className='form--input' type="number" placeholder="Valoracion" {
                     ...register("Valoracion", {required: false})} />
                 {/* Numero de empleados */}
-                <input type="number" placeholder="Numero de empleados" {
+                <input className='form--input' type="number" placeholder="Numero de empleados" {
                     ...register("Numero de empleados", {required: false})} />
                 {/* imagen */}
-                <input type="url" placeholder="Imagen" {
+                <input className='form--input' type="url" placeholder="Imagen" {
                     ...register("Imagen", {required: false})} />
                 {/* Descripcion */}
-                <textarea placeholder="Descripcion" {
+                <textarea className='form--input' placeholder="Descripcion" {
                     ...register("Descripcion", {required: false})} />
 
                 <input className='login--button' type="submit" />
